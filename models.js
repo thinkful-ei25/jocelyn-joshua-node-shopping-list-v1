2e74413910ca1d8a1bc3e42aa56033899b316169
jsantiag <jsantiag@wellesley.edu>
2018-10-10 11:16:11 -0500

    app.post(/recipes)

--- models.js
+++ models.js
@@ -1,3 +1,4 @@
+'use strict';
 const uuid = require('uuid');
 
 // this module provides volatile storage, using a `ShoppingList`
@@ -12,8 +13,8 @@ const uuid = require('uuid');
 
 
 function StorageException(message) {
-   this.message = message;
-   this.name = "StorageException";
+  this.message = message;
+  this.name = 'StorageException';
 }
 
 const ShoppingList = {
@@ -40,7 +41,7 @@ const ShoppingList = {
     const {id} = updatedItem;
     if (!(id in this.items)) {
       throw StorageException(
-        `Can't update item \`${id}\` because doesn't exist.`)
+        `Can't update item \`${id}\` because doesn't exist.`);
     }
     this.items[updatedItem.id] = updatedItem;
     return updatedItem;
@@ -77,7 +78,7 @@ const Recipes = {
     const {id} = updatedItem;
     if (!(id in this.items)) {
       throw StorageException(
-        `Can't update item \`${id}\` because doesn't exist.`)
+        `Can't update item \`${id}\` because doesn't exist.`);
     }
     this.items[updatedItem.id] = updatedItem;
     return updatedItem;
@@ -94,4 +95,4 @@ function createRecipes() {
 module.exports = {
   ShoppingList: createShoppingList(),
   Recipes: createRecipes()
-}
+};
